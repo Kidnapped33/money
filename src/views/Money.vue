57960f7a0a4ea3e03498c344d5720e24a0292a8f
Ashine <404770266@qq.com>
2020-09-08 17:20:39 +0800

    首页布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,6 @@
 <template>
-<div>
-    Money
-    <hr />
+<div class="nav-border">
+    <div class="money">Money</div>
     <Nav />
 </div>
 </template>
@@ -13,4 +12,17 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.nav-border {
+    border: 2px solid green;
+    width: 100vh;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+}
+
+.money {
+    border: 2px solid red;
+    overflow: auto;
+    flex-grow: 1;
+}
 </style>
