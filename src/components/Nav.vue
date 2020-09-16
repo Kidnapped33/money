0bebc3ece7d4960e97ce523049df59751e7006b4
Ashine <404770266@qq.com>
2020-09-16 19:31:19 +0800

    添加全局变量和全局样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -19,6 +19,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
+
 nav {
     display: flex;
     box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
@@ -40,7 +42,7 @@ nav {
     }
 
     .item.selected {
-        color: gray;
+        color: $color-highlight;
     }
 }
 </style>
