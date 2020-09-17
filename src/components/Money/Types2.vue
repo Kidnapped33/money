e068ecd77a81a97dce1c227e939ef88669f730ac
Ashine <404770266@qq.com>
2020-09-17 22:44:51 +0800

    使用 JS 实现了一个组件

--- src/components/Money/Types2.vue
+++ src/components/Money/Types2.vue
@@ -1,15 +1,29 @@
 <template>
-  <div>
+<div>
     <ul class="types">
-      <li class="selected">支出</li>
-      <li>收入</li>
+        <li :class="type==='-'&&'selected'" @click="selectType('-')">支出</li>
+        <li :class="type==='+'&&'selected'" @click="selectType('+')">收入</li>
     </ul>
-  </div>
+</div>
 </template>
 
-<script lang="ts">
+<script>
 export default {
-  name: "Types"
+    name: "Types",
+    data() {
+        return {
+            type: "-" //"-"表示支出，'+'表示收入
+        };
+    },
+    methods: {
+        selectType(type) {
+            //type 只能是“-” 和 “+”中的一个
+            if (type !== "-" && type !== "+") {
+                throw new Error("type is unknown");
+            }
+            this.type = type;
+        }
+    }
 };
 </script>
 
@@ -18,28 +32,28 @@ export default {
 @import "~@/assets/style/reset.scss";
 
 .types {
-  background-color: #c4c4c4;
-  display: flex;
-  text-align: center;
-  font-size: 24px;
-
-  > li {
-    width: 50%;
-    height: 64px;
+    background-color: #c4c4c4;
     display: flex;
-    justify-content: center;
-    align-items: center;
-    position: relative;
+    text-align: center;
+    font-size: 24px;
 
-    &.selected::after {
-      content: "";
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      width: 100%;
-      height: 4px;
-      background-color: #333;
+    >li {
+        width: 50%;
+        height: 64px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: relative;
+
+        &.selected::after {
+            content: "";
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            height: 4px;
+            background-color: #333;
+        }
     }
-  }
 }
 </style>
