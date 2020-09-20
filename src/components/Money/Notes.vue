86b9f7564b1dcd8587171ed8f6f9575bcd947dee
Ashine <404770266@qq.com>
2020-09-20 10:40:19 +0800

    使用 v-model 代替 ：value 和 @input

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -1,22 +1,21 @@
 <template>
-<div>
+  <div>
+    {{value}}
     <label class="notes">
-        <span class="name">备注</span>
-        <input type="text" v-model="value" placeholder="在这里输入备注" />
+      <span class="name">备注</span>
+      <input type="text" v-model="value" placeholder="在这里输入备注" />
     </label>
-</div>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import {
-    Component
-} from "vue-property-decorator";
+import { Component } from "vue-property-decorator";
 
 @Component
 export default class Notes extends Vue {
-    // name: "Notes";
-    value = "";
+  // name: "Notes";
+  value = "";
 }
 </script>
 
@@ -25,22 +24,22 @@ export default class Notes extends Vue {
 @import "~@/assets/style/reset.scss";
 
 .notes {
-    display: flex;
-    font-size: 14px;
-    background-color: #f5f5f5;
-    padding-left: 16px;
-    align-items: center;
+  display: flex;
+  font-size: 14px;
+  background-color: #f5f5f5;
+  padding-left: 16px;
+  align-items: center;
 
-    >.name {
-        padding-right: 16px;
-    }
+  > .name {
+    padding-right: 16px;
+  }
 
-    >input {
-        height: 64px;
-        flex-grow: 1;
-        background: transparent;
-        border: none;
-        padding-right: 16px;
-    }
+  > input {
+    height: 64px;
+    flex-grow: 1;
+    background: transparent;
+    border: none;
+    padding-right: 16px;
+  }
 }
 </style>
