ddd314505cacbeab672c4da2728e44baa1a9c620
Ashine <404770266@qq.com>
2020-10-06 08:11:11 +0800

    将 create 封装到 recordListModel

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,78 +1,80 @@
 <template>
-<Layout class-prefix="layout">
-    <NumberPad :value.sync="record.amount" @sumbit="saveRecord" />
-    <Types :value.sync="record.type" />
-    <div class="notes">
-        <FormItem fieldName="备注"
-                  placeholder="在这里输入备注"
-                  @update:value="onUpdateNotes" />
-    </div>
-    <Tags :data-source.sync="tags" @update:value="onUpdateTags" />
-</Layout>
+    <Layout class-prefix="layout">
+        <NumberPad :value.sync="record.amount" @sumbit="saveRecord"/>
+        <Types :value.sync="record.type"/>
+        <div class="notes">
+            <FormItem fieldName="备注"
+                      placeholder="在这里输入备注"
+                      @update:value="onUpdateNotes"/>
+        </div>
+        <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
+    </Layout>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import NumberPad from "@/components/Money/NumberPad.vue";
-import Tags from "@/components/Money/Tags.vue";
-import FormItem from "@/components/Money/FormItem.vue";
-import Types from "@/components/Money/Types.vue";
-import {
-    Component,
-    Watch
-} from "vue-property-decorator";
-import recordListModel from "@/models/recordListModel";
-import tagListModel from "@/models/tagListModel";
+    import Vue from "vue";
+    import NumberPad from "@/components/Money/NumberPad.vue";
+    import Tags from "@/components/Money/Tags.vue";
+    import FormItem from "@/components/Money/FormItem.vue";
+    import Types from "@/components/Money/Types.vue";
+    import {
+        Component,
+        Watch
+    } from "vue-property-decorator";
+    import recordListModel from "@/models/recordListModel";
+    import tagListModel from "@/models/tagListModel";
 
-const recordList = recordListModel.fetch();
-const tagList = tagListModel.fetch();
+    const recordList = recordListModel.fetch();
+    const tagList = tagListModel.fetch();
 
-@Component({
-    components: {
-        NumberPad,
-        Tags,
-        FormItem,
-        Types
-    }
-})
-export default class Money extends Vue {
-    tags = tagList; //["衣", "食", "住", "行"];
-    recordList: RecordItem[] = recordList;
-    record: RecordItem = {
-        tags: [],
-        notes: "",
-        type: "-",
-        amount: 0
-    };
-    onUpdateTags(value: string[]) {
-        this.record.tags = value;
-    }
-    onUpdateNotes(value: string) {
-        this.record.notes = value;
-    }
+    @Component({
+        components: {
+            NumberPad,
+            Tags,
+            FormItem,
+            Types
+        }
+    })
+    export default class Money extends Vue {
+        tags = tagList; //["衣", "食", "住", "行"];
+        recordList: RecordItem[] = recordList;
+        record: RecordItem = {
+            tags: [],
+            notes: "",
+            type: "-",
+            amount: 0
+        };
 
-    onUpdateAmount(value: string) {
-        this.record.amount = parseFloat(value);
-    }
-    saveRecord() {
-        const record2: RecordItem = recordListModel.clone(this.record);
-        record2.createdAt = new Date();
-        this.recordList.push(record2);
-    }
+        onUpdateTags(value: string[]) {
+            this.record.tags = value;
+        }
 
-    @Watch("recordList")
-    onRecordListChange() {
-        recordListModel.save(this.recordList);
+        onUpdateNotes(value: string) {
+            this.record.notes = value;
+        }
+
+        // onUpdateAmount(value: string) {
+        //     this.record.amount = parseFloat(value);
+        // }
+
+        saveRecord() {
+            recordListModel.create(this.record)
+        }
+
+        @Watch("recordList")
+        onRecordListChange() {
+            recordListModel.save();
+        }
     }
-}
 </script>
 
 <style lang="scss">
-.layout-content {
-    display: flex;
-    flex-direction: column-reverse;
-}
-    .notes{
-        padding: 12px 0 ;
+    .layout-content {
+        display: flex;
+        flex-direction: column-reverse;
+    }
+
+    .notes {
+        padding: 12px 0;
     }
 </style>
